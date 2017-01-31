d76a5bae593c1290869f59ad86b6f31834b75759
protelescristata <gian.silvan.hiltbrunner@gmail.com>
2017-01-31 19:10:46 +0100

    Update song.py

--- song.py
+++ song.py
@@ -1,3 +1,4 @@
+from unidecode import unidecode
 import dbus
 session_bus = dbus.SessionBus()
 spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
@@ -6,5 +7,9 @@ spotify_properties = dbus.Interface(spotify_bus,
                                             "org.freedesktop.DBus.Properties")
 metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 
-# To just print the title
-print metadata['xesam:title'] + " by " + metadata['xesam:artist'][0]
+if (len(metadata['xesam:title']) < 55):#If song name is too long dont print artist
+    print (unidecode(metadata['xesam:title']) + " by " + unidecode(metadata['xesam:artist'][0]))
+elif (len(metadata['xesam:title']) > 70):#If song is too long too display, only disp first 70 chars
+    print (unidecode(metadata['xesam:title'])[:70] + "...")
+else:
+    print (unidecode(metadata['xesam:title']))
