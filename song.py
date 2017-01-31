e8d808b2750dc1054f69bfd487632803b362728e
protelescristata <gian.silvan.hiltbrunner@gmail.com>
2017-01-31 19:48:36 +0100

    Update song.py

--- song.py
+++ song.py
@@ -1,5 +1,9 @@
+LENGTH_NOARTIST = 55 #Dont disp title if longer
+LENGTH_CUT = 70 #Max size of string, cut if longer
+
 from unidecode import unidecode
 import dbus
+
 session_bus = dbus.SessionBus()
 spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
                                              "/org/mpris/MediaPlayer2")
@@ -7,9 +11,9 @@ spotify_properties = dbus.Interface(spotify_bus,
                                             "org.freedesktop.DBus.Properties")
 metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 
-if (len(metadata['xesam:title']) < 55):#If song name is too long dont print artist
+if (len(metadata['xesam:title']) < LENGTH_NOARTIST):
     print (unidecode(metadata['xesam:title']) + " by " + unidecode(metadata['xesam:artist'][0]))
-elif (len(metadata['xesam:title']) > 70):#If song is too long too display, only disp first 70 chars
-    print (unidecode(metadata['xesam:title'])[:70] + "...")
-else:
+elif (len(metadata['xesam:title']) > LENGTH_CUT):#If song is too long too display, only disp first 70 chars
+    print (unidecode(metadata['xesam:title'])[:LENGTH_CUT] + "...")
+else:#If song name is too long dont print artist
     print (unidecode(metadata['xesam:title']))
